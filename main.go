a8e3ed65dad10938503d2121c1535ced4c623f6a
llmerlos <49389868+llmerlos@users.noreply.github.com>
2020-08-25 17:20:46 +0200

    multiple version and variable sets support

--- main.go
+++ main.go
@@ -1,341 +1,42 @@
+// Demo code for the Form primitive.
 package main
 
 import (
-	"archive/zip"
-	"database/sql"
-	"encoding/json"
-	"fmt"
-	"io"
+	"flag"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	var locales = []string{"en_us"}
+	versionPtr := flag.String("v", "latest", "version: X.X.X or latest")
+	flag.Parse()
 
-	if len(os.Args) != 2 {
-		panic("Arguments needed: <version> <nsets>")
-	}
-	nsets64, _ := strconv.ParseInt(os.Args[2], 10, 0)
-	nsets := int(nsets64)
-	version := os.Args[1]
-	dbname := "lor-" + version + ".db"
+	version := *versionPtr
+	tail := flag.Args()
 
-	os.Remove(dbname)
-
-	log.Println("Creating" + dbname + "...")
-	file, err := os.Create(dbname)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	file.Close()
-	log.Println(dbname + " created")
-
-	sqlDb, _ := sql.Open("sqlite3", "./"+dbname)
-	defer sqlDb.Close()
-	CreateTables(sqlDb, SQLTableCreation)
-
-	_, err = os.Stat("downloads")
-	if os.IsNotExist(err) {
-		errDir := os.MkdirAll("downloads", 0755)
-		if errDir != nil {
-			log.Fatal(err)
-		}
-
-	}
-	_, err = os.Stat("downloads" + version)
-	if os.IsNotExist(err) {
-		errDir := os.MkdirAll("downloads/"+version, 0755)
-		if errDir != nil {
-			log.Fatal(err)
-		}
-
-	}
-
-	for _, locale := range Locales {
-		for set := 0; set <= nsets; set++ {
-			var filepath, jsonpath string
-			if set == 0 {
-				filepath = version + "/" + "core-" + locale
-				jsonpath = "downloads/" + filepath + "/" + locale + "/data/" + "globals-" + locale + ".json"
-			} else {
-				filepath = version + "/" + "set" + strconv.Itoa(set) + "-lite-" + locale
-				jsonpath = "downloads/" + filepath + "/" + locale + "/data/" + "set" + strconv.Itoa(set) + "-" + locale + ".json"
-			}
-
-			if locale == Locales[0] {
-				filepath = strings.ReplaceAll(filepath, "-lite", "")
-			}
-			jsonpath = strings.ReplaceAll(jsonpath, "-lite", "")
-			DownloadFile(filepath + ".zip")
-			Unzip("downloads/"+filepath+".zip", strings.ReplaceAll("downloads/"+filepath, "-lite", ""))
-			ParseAndInsert(jsonpath, set, locale, sqlDb)
+	if version != "latest" {
+		r, _ := http.Head(DDUrl + version + "/core-en_us.zip")
+		if r.StatusCode != 200 {
+			log.Fatal("Version " + version + " not found")
 		}
 	}
-}
 
-//DownloadFile downloads a file from the Data Dragon
-func DownloadFile(filepath string) {
-	_, err := os.Stat("downloads/" + filepath)
-	if !os.IsNotExist(err) {
-		return
-	}
-
-	out, err := os.Create("downloads/" + filepath)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	defer out.Close()
-
-	resp, err := http.Get(DDUrl + filepath)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	defer resp.Body.Close()
-
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-}
-
-//Unzip unzip a file
-func Unzip(src, dest string) error {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := r.Close(); err != nil {
-			panic(err)
-		}
-	}()
-
-	os.MkdirAll(dest, 0755)
-
-	extractAndWriteFile := func(f *zip.File) error {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer func() {
-			if err := rc.Close(); err != nil {
-				panic(err)
-			}
-		}()
-
-		path := filepath.Join(dest, f.Name)
-
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(path, f.Mode())
-		} else {
-			os.MkdirAll(filepath.Dir(path), f.Mode())
-			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-			defer func() {
-				if err := f.Close(); err != nil {
-					panic(err)
+	if len(tail) > 0 {
+		for _, locale := range tail {
+			if locale != "en_us" {
+				r, _ := http.Head(DDUrl + version + "/core-" + locale + ".zip")
+				if r.StatusCode == 200 {
+					locales = append(locales, locale)
+				} else {
+					log.Println("Locale: " + locale + " not found")
 				}
-			}()
-
-			_, err = io.Copy(f, rc)
-			if err != nil {
-				return err
 			}
-		}
-		return nil
-	}
-
-	for _, f := range r.File {
-		err := extractAndWriteFile(f)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-//CreateTables creates the tables in the DB
-func CreateTables(db *sql.DB, dbQ []string) {
-	for _, cquery := range dbQ {
-		statement, err := db.Prepare(cquery)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		statement.Exec()
-	}
-}
-
-//ParseAndInsert does this thing
-func ParseAndInsert(file string, set int, locale string, db *sql.DB) {
-
-	jsonStream, err := os.Open(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	global := strings.Contains(file, "globals")
-
-	dec := json.NewDecoder(jsonStream)
-
-	if !global {
-		_, err := dec.Token()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for dec.More() {
-			var c Card
-			err := dec.Decode(&c)
-			if err != nil {
-				log.Fatal(err)
-			}
-			c.Set = "set" + strconv.Itoa(set)
-			fmt.Println(c)
-			insertCard(c, locale, db)
-		}
-
-		_, err = dec.Token()
-		if err != nil {
-			log.Fatal(err)
 		}
 	} else {
-		for dec.More() {
-			var g Global
-			err := dec.Decode(&g)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println(g)
-			insertGeneric(g, locale, db)
-		}
-	}
-}
-
-//insertCard inserts a card to the db
-func insertCard(c Card, locale string, db *sql.DB) {
-	if locale == "en_us" {
-		statement, err := db.Prepare(InsertCardSQLQ)
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-		_, err = statement.Exec(c.CardCode, NullS(c.Set), NullS(c.RegionRef), c.Attack, c.Health, c.Cost, NullS(c.ArtistName), NullS(c.SpellSpeedRef), NullS(c.RarityRef), NullS(c.Subtype), NullS(c.Supertype), NullS(c.Type), c.Collectible)
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-
-		for _, kw := range c.KeywordRefs {
-			statement, err = db.Prepare(InsertKeywordsSQLQ)
-			if err != nil {
-				log.Fatalln(err.Error())
-			}
-			_, err = statement.Exec(c.CardCode, kw)
-			if err != nil {
-				log.Fatalln(err.Error())
-			}
-		}
-
-		for _, as := range c.AssociatedCardRefs {
-			statement, err = db.Prepare(InsertAssociationsSQLQ)
-			if err != nil {
-				log.Fatalln(err.Error())
-			}
-			_, err = statement.Exec(c.CardCode, as)
-			if err != nil {
-				log.Fatalln(err.Error())
-			}
-			_, notok := AssociationsExceptions[c.CardCode]
-			if notok {
-				break
-			}
-		}
-
+		locales = Locales
 	}
 
-	statement, err := db.Prepare(InsertLocaleCardsSQLQ)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	_, err = statement.Exec(c.CardCode, locale, c.Name, NullS(c.Description), NullS(c.DescriptionRaw), NullS(c.LevelupDescription), NullS(c.LevelupDescriptionRaw))
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
+	process(version, locales)
 
-}
-
-func insertGeneric(c Global, locale string, db *sql.DB) {
-	for _, kw := range c.Keywords {
-		statement, err := db.Prepare(InsertLocaleGenericSQLQ)
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-		_, err = statement.Exec("keywords", locale, kw.NameRef, kw.Name, NullS(kw.Description))
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-	}
-
-	for _, vt := range c.VocabTerms {
-		statement, err := db.Prepare(InsertLocaleGenericSQLQ)
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-		_, err = statement.Exec("vocabTerms", locale, vt.NameRef, vt.Name, NullS(vt.Description))
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-	}
-
-	for _, ss := range c.SpellSpeeds {
-		statement, err := db.Prepare(InsertLocaleGenericSQLQ)
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-		_, err = statement.Exec("spellSpeeds", locale, ss.NameRef, ss.Name, sql.NullString{})
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-	}
-
-	for _, ra := range c.Rarities {
-		statement, err := db.Prepare(InsertLocaleGenericSQLQ)
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-		_, err = statement.Exec("spellSpeeds", locale, ra.NameRef, ra.Name, sql.NullString{})
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-	}
-
-	for _, re := range c.Regions {
-		statement, err := db.Prepare(InsertLocaleGenericSQLQ)
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-		_, err = statement.Exec("vocabTerms", locale, re.NameRef, re.Name, NullS(re.Description))
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-	}
-
-}
-
-//NullS inserts null instead of empty string
-func NullS(s string) sql.NullString {
-	if len(s) == 0 || s == " " {
-		return sql.NullString{}
-	}
-	return sql.NullString{
-		String: s,
-		Valid:  true,
-	}
 }
---
+++ utils.go
@@ -0,0 +1,367 @@
+package main
+
+import (
+	"archive/zip"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+//Do the main work utside the UI thread
+func process(version string, locales []string) {
+	nsets := 1
+	for true { //Checks for the existance of sets given a version
+		r, err := http.Head(DDUrl + version + "/set" + strconv.Itoa(nsets) + "-en_us.zip")
+		fmt.Println(r.StatusCode)
+		fmt.Println(err)
+		if r.StatusCode != 200 {
+			nsets--
+			break
+		} else {
+			nsets++
+		}
+	}
+
+	//Create empty database (erase if exists)
+	dbname := "lor-" + version + ".db"
+	log.Println("Creating " + dbname)
+	os.Remove(dbname)
+	file, err := os.Create(dbname)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	file.Close()
+
+	//Open database and create tables
+	log.Println("Opening " + dbname)
+	sqlDb, _ := sql.Open("sqlite3", "./"+dbname)
+	defer sqlDb.Close()
+	log.Println("Creating tables in " + dbname)
+	createTables(sqlDb, SQLTableCreation)
+
+	//Make download directory if it doesn't exist
+	log.Println("Making downloads directory")
+	_, err = os.Stat("downloads")
+	if os.IsNotExist(err) {
+		errDir := os.MkdirAll("downloads", 0755)
+		if errDir != nil {
+			log.Fatal(err)
+		}
+
+	}
+
+	//Make version directory if it doesn't exist
+	log.Println("Making version directory")
+	_, err = os.Stat("downloads" + version)
+	if os.IsNotExist(err) {
+		errDir := os.MkdirAll("downloads/"+version, 0755)
+		if errDir != nil {
+			log.Fatal(err)
+		}
+	}
+
+	//Iterate for every locale in core and each set
+	for _, locale := range locales {
+		for set := 0; set <= nsets; set++ {
+			var filepath, jsonpath string
+			if set == 0 { //There's no set0 instead its the core bundle
+				filepath = version + "/" + "core-" + locale
+				jsonpath = "downloads/" + filepath + "/" + locale + "/data/" + "globals-" + locale + ".json"
+			} else { //Instead of downloading the full bundle the lite suffices
+				filepath = version + "/" + "set" + strconv.Itoa(set) + "-lite-" + locale
+				jsonpath = "downloads/" + filepath + "/" + locale + "/data/" + "set" + strconv.Itoa(set) + "-" + locale + ".json"
+				jsonpath = strings.ReplaceAll(jsonpath, "-lite", "") //ease of constructing the string
+			}
+
+			if locale == "en_us" { //For the en_us locale we download the full bundle to have the full art
+				filepath = strings.ReplaceAll(filepath, "-lite", "")
+			}
+			log.Println("Downloading " + filepath + ".zip")
+			downloadFile(filepath + ".zip")
+			log.Println("Unziping " + filepath + ".zip")
+			unzip("downloads/"+filepath+".zip", strings.ReplaceAll("downloads/"+filepath, "-lite", ""))
+			log.Println("Parsing and inserting " + jsonpath)
+			parseAndInsert(jsonpath, set, locale, sqlDb)
+		}
+	}
+}
+
+//download a file given the filepath to it in the DD
+func downloadFile(filepath string) {
+
+	//if the file already exists don't download it again
+	_, err := os.Stat("downloads/" + filepath)
+	if !os.IsNotExist(err) {
+		return
+	}
+
+	//create empty file
+	out, err := os.Create("downloads/" + filepath)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer out.Close()
+
+	//make the request
+	resp, err := http.Get(DDUrl + filepath)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer resp.Body.Close()
+
+	//transfer the files from the request to the file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+//unzip from a source file to a destination path
+func unzip(src, dest string) error {
+	//open file
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	//create destination folder with appropiate perms
+	os.MkdirAll(dest, 0755)
+
+	//extract the file there
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		path := filepath.Join(dest, f.Name)
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+//execute an array of create table queries on a given database
+func createTables(db *sql.DB, dbQ []string) {
+	for _, cquery := range dbQ {
+		statement, err := db.Prepare(cquery)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		statement.Exec()
+	}
+}
+
+//given a JSON file, a set, a locale and a databse parse the info and insert it
+func parseAndInsert(file string, set int, locale string, db *sql.DB) {
+	jsonStream, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dec := json.NewDecoder(jsonStream)
+	global := strings.Contains(file, "globals")
+	//the global/core file has a different structure so it has the be parsed differently
+	if !global {
+		_, err := dec.Token() //takes out first token "["
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for dec.More() { //while it has more items keep parsing
+			var c Card
+			err := dec.Decode(&c)
+			if err != nil {
+				log.Fatal(err)
+			}
+			c.Set = "set" + strconv.Itoa(set) //Set has the be inserted manually because the json doesn't provide the info
+			s, _ := json.MarshalIndent(c, "", "\t")
+			log.Println(string(s))
+			insertCard(c, locale, db) // insert on the database
+		}
+
+		_, err = dec.Token()
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else { // does the same as above but doesnt remove the token
+		for dec.More() {
+			var g Global
+			err := dec.Decode(&g)
+			if err != nil {
+				log.Fatal(err)
+			}
+			s, _ := json.MarshalIndent(g, "", "\t")
+			log.Println(string(s))
+			insertGeneric(g, locale, db)
+		}
+	}
+}
+
+//set of queries to perform for each card
+func insertCard(c Card, locale string, db *sql.DB) {
+	if locale == "en_us" { //the base stats are only inserted with "en_us" locale
+		statement, err := db.Prepare(InsertCardSQLQ)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		//cards table
+		_, err = statement.Exec(c.CardCode, nullS(c.Set), nullS(c.RegionRef), c.Attack, c.Health, c.Cost, nullS(c.ArtistName), nullS(c.SpellSpeedRef), nullS(c.RarityRef), nullS(c.Subtype), nullS(c.Supertype), nullS(c.Type), c.Collectible)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		//keywords table
+		for _, kw := range c.KeywordRefs {
+			statement, err = db.Prepare(InsertKeywordsSQLQ)
+			if err != nil {
+				log.Fatalln(err.Error())
+			}
+			_, err = statement.Exec(c.CardCode, kw)
+			if err != nil {
+				log.Fatalln(err.Error())
+			}
+		}
+		//associations table
+		for _, as := range c.AssociatedCardRefs {
+			statement, err = db.Prepare(InsertAssociationsSQLQ)
+			if err != nil {
+				log.Fatalln(err.Error())
+			}
+			_, err = statement.Exec(c.CardCode, as)
+			if err != nil {
+				log.Fatalln(err.Error())
+			}
+			_, notok := AssociationsExceptions[c.CardCode] //Some cards have multiple times the same card associated triggering an error of uniqueness
+			if notok {
+				break
+			}
+		}
+
+	}
+	//insert the translations for the locale
+	statement, err := db.Prepare(InsertLocaleCardsSQLQ)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(c.CardCode, locale, c.Name, nullS(c.Description), nullS(c.DescriptionRaw), nullS(c.LevelupDescription), nullS(c.LevelupDescriptionRaw))
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+}
+
+//set of queries to perform for each core
+func insertGeneric(c Global, locale string, db *sql.DB) {
+	//localeGeneric table
+	for _, kw := range c.Keywords {
+		statement, err := db.Prepare(InsertLocaleGenericSQLQ)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		_, err = statement.Exec("keywords", locale, kw.NameRef, kw.Name, nullS(kw.Description))
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+	}
+
+	for _, vt := range c.VocabTerms {
+		statement, err := db.Prepare(InsertLocaleGenericSQLQ)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		_, err = statement.Exec("vocabTerms", locale, vt.NameRef, vt.Name, nullS(vt.Description))
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+	}
+
+	for _, ss := range c.SpellSpeeds {
+		statement, err := db.Prepare(InsertLocaleGenericSQLQ)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		_, err = statement.Exec("spellSpeeds", locale, ss.NameRef, ss.Name, sql.NullString{})
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+	}
+
+	for _, ra := range c.Rarities {
+		statement, err := db.Prepare(InsertLocaleGenericSQLQ)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		_, err = statement.Exec("spellSpeeds", locale, ra.NameRef, ra.Name, sql.NullString{})
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+	}
+
+	for _, re := range c.Regions {
+		statement, err := db.Prepare(InsertLocaleGenericSQLQ)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		_, err = statement.Exec("vocabTerms", locale, re.NameRef, re.Name, nullS(re.Description))
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+	}
+
+}
+
+//to insert a null string instead of an empty string when needed
+func nullS(s string) sql.NullString {
+	if len(s) == 0 || s == " " {
+		return sql.NullString{}
+	}
+	return sql.NullString{
+		String: s,
+		Valid:  true,
+	}
+}
